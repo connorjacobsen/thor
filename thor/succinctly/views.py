2ca7edd1b68d1d079cdc63f31752d82dcb40b42f
Connor Jacobsen <jacobsen.connor@gmail.com>
2013-06-23 21:27:52 -0400

    Updated detail and index views

--- thor/succinctly/views.py
+++ thor/succinctly/views.py
@@ -1,8 +1,13 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from succinctly.models import Article
 
 def index(request):
-	return HttpResponse('Hello, world!')
+	article_list = Article.objects.order_by('-pub_date')[:10]
+	context = {'article_list': article_list}
+	return render(request, 'succinctly/index.html', context)
 
 def detail(request, article_id):
-	return HttpResponse("You are looking at article %s." % article_id)	
+	article = get_object_or_404(Article, pk=article_id)
+	return render(request, 'succinctly/detail.html', {'article': article})	
