eace4311e79ae66024bb362fb1f44a0da7d37679
Connor Jacobsen <jacobsen.connor@gmail.com>
2013-06-25 22:07:25 -0400

    Built v1 of the authentication and registration system. Still needs to be tested (extensively) and actually used to register and manipulate users.

---
+++ thor/accounts/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'CustomUser'
+        db.create_table(u'accounts_customuser', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('password', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('last_login', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('email', self.gf('django.db.models.fields.EmailField')(unique=True, max_length=255, db_index=True)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_admin', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal(u'accounts', ['CustomUser'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'CustomUser'
+        db.delete_table(u'accounts_customuser')
+
+
+    models = {
+        u'accounts.customuser': {
+            'Meta': {'object_name': 'CustomUser'},
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['accounts']
--- thor/accounts/models.py
+++ thor/accounts/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+	def create_user(self, email, password):
+		"""
+		Creates and saves a User with the given email, first and last 
+		names, and password.
+		"""
+		if not email:
+			raise ValueError('Users must have a valid email address')
+
+		if not password:
+			raise ValueError('Users must have a valid password')
+
+		user = self.model(
+			email = MyUserManager.normalize_email(email),
+			#user.set_password(password)
+			)
+		user.set_password(password)
+
+		# if first_name:
+		#	user.first_name = first_name
+		# if last_name:
+		#	user.last_name = last_name	
+
+		user.save(using=self.db)
+		return user
+
+	def create_superuser(self, email, password):	
+		"""
+		Creates and saves a superuser with the given email, name, 
+		and password.
+		"""
+		user = self.create_user(email, 
+			password, 
+		)
+		user.is_admin = True
+		user.save(using = self.db)
+
+class CustomUser(AbstractBaseUser):
+	email = models.EmailField(
+		verbose_name = 'email address',
+		max_length = 255,
+		unique = True,
+		db_index = True,
+	)					
+	# It may not be necessary to declare a password attribute
+	# password = models.PasswordField()
+	is_active = models.BooleanField(default = True)
+	is_admin = models.BooleanField(default = False)
+
+	objects = MyUserManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+
+	def get_full_name(self):
+		# The user is identified by their email address
+		return self.email
+
+	def get_short_name(self):
+		# The user is identified by their email address
+		return self.email
+
+	def __unicode__(self):
+		return self.email
+
+	# Consider editing permissions so that admin is not accessible 
+	def has_perm(self, perm, obj=None):
+		"Does the user have a specific permission?"
+		# Simplest possible answer: Yes, always
+		return True
+
+	def has_module_perm(self, app_label):
+		"Does the user have permissions to view the app `app_label`?"
+		# Simplest answer: Yes, always
+		return True				
+
+	@property 
+	def is_staff(self):
+		"Is the user a member of staff?"
+		# If the user is an admin, they are staff
+		return self.is_admin
+		
